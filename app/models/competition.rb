518fee1e4fc564b621249abcb6c09495fcba6fcb
thorncp <thorncp@gmail.com>
2012-06-05 23:13:49 -0800

    Competition validations

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,15 @@
 class Competition < ActiveRecord::Base
   belongs_to :contact, class_name: "User", foreign_key: "user_id"
   attr_accessible :description, :end_date, :name, :start_date, :contact
+
+  validates_presence_of :name, :description, :start_date, :end_date, :contact
+  validate :end_date_cannot_be_before_start_date
+
+  private
+
+  def end_date_cannot_be_before_start_date
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before start date")
+    end
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Competition do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "should not allow start date after end date" do
+      subject.start_date = 1.week.from_now
+      subject.end_date = 1.week.ago
+      subject.should have(1).error_on(:end_date)
+    end
+  end
 end
