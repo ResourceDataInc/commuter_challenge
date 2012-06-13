6f1041333c17c1c936ad54866f25566b67bc0420
thorncp <thorncp@gmail.com>
2012-06-12 23:31:25 -0800

    Minor work on competition show page

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,9 +3,13 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :competitions
   has_and_belongs_to_many :cyclists, class_name: "User", :association_foreign_key => "user_id",
       :join_table => "teams_users"
-  
+
   attr_accessible :description, :name, :captain, :user_id, :business_size
-  
+
   validates :name, :uniqueness => true
   validates_presence_of :name, :business_size
+
+  def self.in_range(lower, upper)
+    where "business_size between ? and ?", lower, upper
+  end
 end
