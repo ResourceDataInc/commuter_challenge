1d3bfd074a7b0ea49f344cb47b453b62bae2d8e4
boonrs <becky.sue.boone@gmail.com>
2012-06-12 21:48:05 -0800

    Update seed description

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts 'New user created: ' << user.name
 user.add_role :admin
 
 puts "SETTING UP DEFAULT COMPETITION"
-competition = Competition.create! name: "Summer Bike Challenge", description: "Friendly competition to encourage bicycle commuting and see which businesses can get the greatest percentage of employees commuting to work by bike.", start_date: Date.parse("21/05/2012"), end_date: Date.parse("17/08/2012"), contact: user
+competition = Competition.create! name: "2012 BCA Summer Bike Challenge", description: "Friendly competition to encourage bicycle commuting and see which businesses can get the greatest percentage of employees commuting to work by bike.  http://bicycleanchorage.org/wordpress/?page_id=2038", start_date: Date.parse("21/05/2012"), end_date: Date.parse("17/08/2012"), contact: user
 puts "New competition created " + competition.name
 
 puts "SETTING UP DEFAULT BUSINESS SIZES"
