1e06b2fdee480b830d38ea8f1961d179e5031db2
Chris Thorn <thorncp@gmail.com>
2013-04-03 15:12:26 -0800

    Add alerts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    flash[:success] = "Success!"
+    flash[:error] = "Error!"
+    flash[:warning] = "Warning!"
+    flash[:info] = "Info!"
   end
 end
