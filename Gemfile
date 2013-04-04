f5d2676908f725e9f768d990bc368cfeb6bc0c10
Chris Thorn <thorncp@gmail.com>
2013-04-03 19:56:10 -0800

    Clean up spec

--- Gemfile
+++ Gemfile
@@ -20,4 +20,5 @@ group :development, :test do
   gem 'shoulda-matchers', '~> 1.5'
   gem 'rspec-rails', '~> 2.13'
   gem 'capybara', '~> 2.0'
+  gem 'factory_girl_rails'
 end
--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -1,63 +1,58 @@
 require "spec_helper"
 
-describe "User" do
-  it "can sign up for an account" do
-    visit root_url
-    click_on "Sign up"
-    fill_in "user_email", with: "test@example.com"
-    fill_in "user_username", with: "Test User"
-    fill_in "user_password", with: "pwd4test"
-    fill_in "user_password_confirmation", with: "pwd4test"
-    click_button "Sign up"
+describe "a user" do
+  let(:user) { FactoryGirl.create :user, username: "Test User" }
 
-    page.should have_content "Test User"
-    page.should have_link "Sign out"
-    page.should_not have_link "Sign up"
-    page.should_not have_link "Sign in"
+  context "not signed in" do
+    before(:each) { visit root_url }
+
+    it "can sign up for an account" do
+      click_on "Sign up"
+      fill_in "user_email", with: "test@example.com"
+      fill_in "user_username", with: "Test User"
+      fill_in "user_password", with: "pwd4test"
+      fill_in "user_password_confirmation", with: "pwd4test"
+      click_button "Sign up"
+
+      page.should have_content "Test User"
+      page.should have_link "Sign out"
+      page.should_not have_link "Sign up"
+      page.should_not have_link "Sign in"
+    end
+
+    it "can sign in" do
+      click_on "Sign in"
+      fill_in "user_email", with: "test@example.com"
+      fill_in "user_password", with: user.password
+      click_button "Sign in"
+      page.should have_content "Test User"
+      page.should have_link "Sign out"
+      page.should_not have_link "Sign in"
+      page.should_not have_link "Sign up"
+    end
   end
 
-  it "can log in" do
-    user = User.create! email: "test@example.com",
-                        username: "Test User",
-                        password: "pwd4test",
-                        password_confirmation: "pwd4test"
-    visit root_url
-    click_on "Sign in"
-    fill_in "user_email", with: "test@example.com"
-    fill_in "user_password", with: "pwd4test"
-    click_button "Sign in"
-    page.should have_content "Test User"
-    page.should have_link "Sign out"
-    page.should_not have_link "Sign in"
-    page.should_not have_link "Sign up"
-  end
+  context "signed in" do
+    before(:each) do
+      login_as user
+      visit root_url
+    end
 
-  it "can sign in" do
-    user = User.create! email: "test@example.com",
-                        username: "Test User",
-                        password: "pwd4test",
-                        password_confirmation: "pwd4test"
-    login_as user
-    visit root_url
-    click_on "Sign out"
-    page.should_not have_content "Test User"
-    page.should_not have_link "Sign out"
-    page.should have_link "Sign up"
-    page.should have_link "Sign in"
-  end
+    it "can sign out" do
+      click_on "Sign out"
+      page.should_not have_content "Test User"
+      page.should_not have_link "Sign out"
+      page.should have_link "Sign up"
+      page.should have_link "Sign in"
+    end
 
-  it "can edit account info" do
-    user = User.create! email: "test@example.com",
-                        username: "Test User",
-                        password: "pwd4test",
-                        password_confirmation: "pwd4test"
-    login_as user
-    visit root_url
-    click_on user.username
-    fill_in "user_email", with: "changed@example.com"
-    fill_in "user_username", with: "Changed User"
-    fill_in "user_current_password", with: "pwd4test"
-    click_on "Update"
-    page.should have_content "Changed User"
+    it "can edit account info" do
+      click_on user.username
+      fill_in "user_email", with: "changed@example.com"
+      fill_in "user_username", with: "Changed User"
+      fill_in "user_current_password", with: "pwd4test"
+      click_on "Update"
+      page.should have_content "Changed User"
+    end
   end
 end
