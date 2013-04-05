e0120cadfdd337292892ab3386291bad80bd5091
Chris Thorn <thorncp@gmail.com>
2013-04-05 11:09:27 -0800

    Add competition abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user = nil)
-    can :read, :secret if user.present?
+    can :read, Competition
+
+    if user.present?
+      can :read, :secret
+
+      can :manage, Competition, owner_id: user.id
+    end
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -2,16 +2,41 @@ require "spec_helper"
 
 describe Ability do
   context "for anonymous user" do
+    subject(:ability) { Ability.new(nil) }
+
     it "can't read the secret" do
-      ability = Ability.new(nil)
       ability.should_not be_able_to :read, :secret
+    end
+
+    it "can read all competitions" do
+      ability.should be_able_to :read, Competition
+    end
+
+    it "cannot create a competition" do
+      ability.should_not be_able_to :create, Competition
     end
   end
 
   context "for authenticated user" do
+    let(:user) { FactoryGirl.create(:user) }
+    subject(:ability) { Ability.new(user) }
+
     it "can read the secret" do
-      ability = Ability.new(FactoryGirl.create(:user))
       ability.should be_able_to :read, :secret
+    end
+
+    it "can create a competition" do
+      ability.should be_able_to :create, Competition
+    end
+
+    it "can manage own competition" do
+      competition = FactoryGirl.create :competition, owner: user
+      ability.should be_able_to :manage, competition
+    end
+
+    it "cannot manage another user's competition" do
+      competition = FactoryGirl.create :competition
+      ability.should_not be_able_to :manage, competition
     end
   end
 end
