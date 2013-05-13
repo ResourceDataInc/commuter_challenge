4801539b726abf252947829650a54f329f44596e
Becky <becky.sue.boone@gmail.com>
2013-05-12 21:15:57 -0800

    Use built in reset_counters method

--- db/migrate/20130512193604_add_competitors_count_to_competitions.rb
+++ db/migrate/20130512193604_add_competitors_count_to_competitions.rb
@@ -2,8 +2,7 @@ class AddCompetitorsCountToCompetitions < ActiveRecord::Migration
   def change
     add_column :competitions, :competitors_count, :integer, :default=>0
     Competition.find_each do |competition|
-      competition.update_attribute(:competitors_count, competition.competitors.length)
-      competition.save
+      Competition.reset_counters(competition.id, :competitors)
     end
   end
 end
--- db/migrate/20130512193622_add_memberships_count_to_teams.rb
+++ db/migrate/20130512193622_add_memberships_count_to_teams.rb
@@ -2,8 +2,7 @@ class AddMembershipsCountToTeams < ActiveRecord::Migration
   def change
     add_column :teams, :memberships_count, :integer, :default=>0
     Team.find_each do |team|
-      team.update_attribute(:memberships_count, team.memberships.length)
-      team.save
+      Team.reset_counters(team.id, :memberships)
     end
   end
 end
