348205c46b49b7c56b82b24087727197b0b27016
Becky <becky.sue.boone@gmail.com>
2013-05-15 17:07:52 -0800

    Cascading deletes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,16 @@ describe User do
     it { should have_many :memberships }
     it { should have_many :teams }
     it { should have_many :rides }
+
+    it "should delete rides when deleted" do
+      user = FactoryGirl.create(:user)
+      id = user.id
+      user.rides.create(rider_id: user.id, distance: 5, date: Date.today)
+
+      user.destroy
+      user = Ride.find_by_rider_id(id)
+      user.should be_nil
+    end
   end
 
   describe "validation" do
