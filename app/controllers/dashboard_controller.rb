f9485dab5a9a2ddb82a39917525c0ede9c301b08
Richard <rlgnak@gmail.com>
2013-05-23 18:30:41 -0800

    cleanup

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,10 @@
-include Warden::Test::Helpers
 class DashboardController < ApplicationController
-  #authorize_resource :class => false
+  authorize_resource :class => false
 
   def index
-    login_as User.find(4)
     @ride = current_user.rides.first
     @ride.date = Date.today
     @ride.is_round_trip = true
+    @rides = current_user.rides.first(9);
   end
 end
