cf491a3718e273d49ebda7b25133c69853274345
Chris Thorn <thorncp@gmail.com>
2014-05-09 10:58:30 -0700

    Extract active header link method
    Also changes how the active-ness is determined, from using the
    controller name, to looking at the current url.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def title(content)
     content_for :title, content
   end
+
+  def header_link_to(body, url)
+    klass = request.path.include?(url) ? "active" : nil
+    content_tag :li, class: klass do
+      link_to body, url
+    end
+  end
 end
