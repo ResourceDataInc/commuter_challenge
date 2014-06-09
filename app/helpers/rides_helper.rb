debea13c63b7982616ba3215d3c5b41c7b1465e5
Becky Boone <becky.sue.boone@gmail.com>
2014-06-08 18:19:04 -0700

    Add personal or work ride info
    Helps keep other rider's data transparent when people get competitive and look at other data

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -2,4 +2,12 @@ module RidesHelper
   def trip_type(ride)
     I18n.t("ride.options.#{ride.type}")
   end
+
+  def work_or_personal(work)
+    if(work)
+      I18n.t("ride.options.work_trip")
+    else
+      I18n.t("ride.options.personal_trip")
+    end
+  end
 end
